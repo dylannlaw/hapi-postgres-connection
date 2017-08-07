11058ca8ee4a3e712e75cc9ce9559ec3d95a0f58
ddrager <ddrager@gmail.com>
2017-08-07 14:34:52 -0400

    Updates connection string for test

--- test/_create_test_db.js
+++ test/_create_test_db.js
@@ -7,7 +7,7 @@ var pg = require('pg');
 var assert = require('assert');
 
 function create_tables (callback) {
-  var pool = new pg.Pool(process.env.DATABASE_URL);
+  var pool = new pg.Pool({connectionString: process.env.DATABASE_URL});
   pool.connect( function(err, client) {
     assert(!err); // if db connection fails then EXPLODE!!
     var file = require('path').resolve(__dirname + '/test_db_setup.sql');
